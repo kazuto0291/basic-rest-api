a09573caa4578829706860eecb66b66dd2f6328d
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-01 20:51:32 +0900

    user-search

---
+++ app/public/js/index.js
@@ -0,0 +1,11 @@
+// 即時関数でモジュール化
+
+const indexModule = (() => {
+    // 検索ボタンをクリックした時のイベントリスナー設定
+    document.getElementById('search-btn').addEventListener('click', () => {
+        return searchModule.searchUsers()
+    })
+
+    //UsersモジュールのfetchAllUsersメソッドを呼び出す
+    return usersModule.fetchAllUsers()
+})()
