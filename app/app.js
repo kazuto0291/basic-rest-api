f267deedbc29ae23cce1c77c2f1ca2360949cd1b
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-03 19:45:17 +0900

    following users API作成

--- app/app.js
+++ app/app.js
@@ -156,9 +156,31 @@ app.delete(`/api/v1/users/:id`, async (req, res) => {
     }
   })
 
-
   db.close()
 })
+
+// Get following users
+app.get(`/api/v1/users/:id/following`, (req, res) => {
+  // connect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  db.all(`SELECT *
+          FROM following
+          LEFT JOIN users
+          ON following.followed_id = users.id
+          WHERE following_id = ${id};`
+          , (err, rows) => {
+          if (!rows) {
+            res.status(404).send({error:"Not Found!"})
+          } else {
+            res.status(200).json(rows)
+          }
+  })
+  db.close()
+})
+
+
 
 
 const port = process.env.PORT || 3000;
